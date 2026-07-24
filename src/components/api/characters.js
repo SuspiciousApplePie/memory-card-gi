09a48105ce797081995bd73945e855413f7f8687
SuspiciousApplePie <andreiusita6@gmail.com>
2026-07-24 21:56:44 +0800

    Add character fetching logic

---
+++ src/components/api/characters.js
@@ -0,0 +1,22 @@
+async function getCharacters(charNames) {
+  try {
+    const characterData = [];
+    for (const charName of charNames) {
+      console.log(charName);
+      const res = await fetch(
+        `https://genshin.jmp.blue/characters/${charName}/`,
+      );
+      if (!res.ok) {
+        throw new Error("Failed to fetch character data");
+      }
+      const characterDetail = await res.json();
+      const { id, name, title, vision } = characterDetail;
+      characterData.push({ id, name, title, vision });
+    }
+    return characterData;
+  } catch (error) {
+    return error;
+  }
+}
+
+export { getCharacters };

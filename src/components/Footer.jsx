16574443b6d7ef5d5711b0ea5fbb36d30edd1b0a
SuspiciousApplePie <andreiusita6@gmail.com>
2026-08-02 21:59:47 +0800

    Remove footer

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer({ setAboutPageStatus }) {
-  return (
-    <footer>
-      <button onClick={() => setAboutPageStatus(true)}>About</button>
-    </footer>
-  );
-}
-
-export { Footer };

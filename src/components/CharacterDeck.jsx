c7c39de50738d2df91137c70258b46950f929a6c
SuspiciousApplePie <andreiusita6@gmail.com>
2026-08-04 19:44:02 +0800

    Add font size responsivity depending on width

--- src/components/CharacterDeck.jsx
+++ src/components/CharacterDeck.jsx
@@ -44,7 +44,7 @@ function CharacterDeck({
         if (ignore) return;
         if (charDeck.length === 0) {
           const shuffled = shuffle([...res]);
-          setCharDeck([...shuffled].slice(0, 10));
+          setCharDeck([...shuffled]);
         }
       })
       .catch((error) => {
